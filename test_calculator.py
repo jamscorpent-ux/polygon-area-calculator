334939abfa9ffc868338c9b3d4b3e5300dc21e36
jamscorpent-ux <jams.corpent@gmail.com>
2026-05-24 13:43:36 +0800

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,2 +1,5 @@
-def test_dummy():
-    assert True     
+import kalkulator 
+
+def test_kalkulator_wujud():
+    # 2. Ujian untuk pastikan fail kalkulator anda berjaya dibaca tanpa error
+    assert hasattr(kalkulator, '__file__')
